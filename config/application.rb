b9afc3952de06fc7d9332588c3b33e02b9674f67
ankit67776 <ankitraikwal0207#gmail.com>
2024-12-30 14:29:56 +0000

    signup component added

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Social
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
+    config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
